85336d3c7973b6bc1357144a0861d3c9be84f377
MrKustik <146933683+MrKustik@users.noreply.github.com>
2024-12-10 16:08:08 +0200

    Add files via upload

---
+++ порождающие паттерны/Singletone/ShawarmaSingletone.cs
@@ -0,0 +1,37 @@
+﻿namespace Singletone
+{
+    public class ShawarmaSingletone
+    {
+        private string Meat { get; }
+        private string Souce { get; }
+        private string Cheese { get; }
+        private string Lavash { get; }
+
+        private ShawarmaSingletone(string meat, string souce, string cheese, string lavash)
+        {
+            Meat = meat;
+            Souce = souce;
+            Cheese = cheese;
+            Lavash = lavash;
+        }
+
+        private static ShawarmaSingletone _instance;
+
+        public static ShawarmaSingletone Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = readConfig();
+                }
+                return _instance;
+            }
+        }
+
+        private static ShawarmaSingletone readConfig()
+        {
+            return new ShawarmaSingletone("beef", "yogurt", "parmezan", "lavash");
+        }
+    }
+}
